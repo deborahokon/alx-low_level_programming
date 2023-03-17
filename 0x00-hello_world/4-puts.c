879b7299f10c7d113ffba6c7338fe386b4bec4f9
deborahokon <deborahokon661@gmail.com>
2023-03-17 08:17:53 -0700

    low level

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This program outputs a text to the screen using the puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
